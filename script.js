40afd5ea7bb2bd6beeb259e3db2ab26a5fcc6ba6
Manas khandelwal <52570042+mkosssd@users.noreply.github.com>
2023-04-03 00:28:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 let url = "https://api.coinlore.net/api/exchange/?id=5";
 let response= fetch(url);
 response.then((crypto)=>{
-        return crypto.json();
+    return crypto.json();
     }).then((crypto)=>{
-        // console.log(crypto)
+        console.log(crypto)
         ihtml="";
     for (let key in crypto.pairs) {
         var epoch = crypto.pairs[key].time;
@@ -16,16 +16,16 @@ response.then((crypto)=>{
       <span>/${crypto.pairs[key].quote}</span> 
       <p>VOLUME: ${crypto.pairs[key].volume}</p>
      <p>PRICE:${crypto.pairs[key].price_usd}</p>
-     <p class="foot">LAST UPDATED AT ${date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()}</p>
-  </div>`
-
-
-
-
+     </div>`
+     
+     
+     
     }
+    // <p class="foot">LAST UPDATED AT ${date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()}</p>
+    time.innerHTML=`TIME LAST UPDATED: ${date.getHours()< 10?'0':''+date.getHours()}${date.getMinutes() < 10 ? '0' : '' + date.getMinutes()} HOURS, on ${date.toDateString()}`
    
     prices.innerHTML = ihtml
-}).catch((err)=>{
+}).catch((err)=>{   
     console.log(err);
 
 })
